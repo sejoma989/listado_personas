a208f6e4ef82868ff36b255e9cadea43b4d735d8
jose_pro <sejoma989@gmail.com>
2022-10-11 21:09:22 -0500

    feat: form to add new Persona, button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // console.log('Archivo app.js loaded');
 const personas = [
-    new Persona('Joselo', 'chuporico'),
+    new Persona('Joselo', 'Chuporico'),
     new Persona('Armando', 'Parches')
 ];
 
@@ -12,4 +12,18 @@ function mostrarPersonas(){
         salidaTexto += `<li>${persona.nombre} ${persona.apellido}</li>`;        
     }
     document.getElementById('personas').innerHTML = salidaTexto;
+}
+
+function agregarPersona(){
+    const forma = document.forms['forma'];
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+    if (nombre.value != '' && apellido.value != ''){
+        const persona = new Persona(nombre.value, apellido.value);
+        console.log(persona);
+        personas.push(persona);
+        mostrarPersonas();
+    } else{
+        console.log('No hay informacion que agregar');
+    }
 }
