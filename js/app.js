eec3b21b7ddc61fc14ecf11f512a1a3cc9c5b648
jose_pro <sejoma989@gmail.com>
2022-10-11 20:12:03 -0500

    feat: create Personas list object

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('Archivo app.js loaded');
+// console.log('Archivo app.js loaded');
+const personas = [
+    new Persona('Joselo', 'chuporico'),
+    new Persona('Armando', 'Parches')
+];
+
+function mostrarPersonas(){
+    console.log('Ejecutando mostrarPersonas()...');
+    let salidaTexto = '';
+    for (let persona of personas){
+        console.log(persona);
+        salidaTexto += `<li>${persona.nombre} ${persona.apellido}</li>`;        
+    }
+    document.getElementById('personas').innerHTML = salidaTexto;
+}
